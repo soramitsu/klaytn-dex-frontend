89fd313a24f2bf91eec3b84b42df92a06e6630f1
RDMStreet <RDMStreetOne@gmail.com>
2022-06-17 00:01:45 +0300

    [Feat] [KLD-76] Soramitsu-ui notifications integration

---
+++ src/components/NotificationsInit.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts" name="NotificationsInit">
+import { useNotifications } from '@soramitsu-ui/ui'
+
+const { show } = useNotifications()
+
+window.$notify = show
+</script>
+
+<template />
